a3e76a827bce0e8adbe8f6a8893142a58b4e697f
Blake Balbirnie <blakebalbirnie@gmail.com>
2018-01-08 15:10:12 +0000

    automatically backed up by learn

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -3,10 +3,6 @@ def rescue_princess_peach
   puts "Hooray! Mario has rescued Princess Peach."
 end
 
-def using(yes)
-  puts yes
-end
-
 
 ## Comment the below back in to see method scope in action!
 
